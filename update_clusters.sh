027f406e11cd6650d925e5bf70134b35ac245bf4
Michele Spagnuolo <mikispag@gmail.com>
2017-04-03 20:20:14 +0200

    Update update_clusters.sh

--- update_clusters.sh
+++ update_clusters.sh
@@ -1,38 +1,62 @@
 #!/bin/bash
 
+BLOCKPARSER="/usr/local/bin/rusty-blockparser"
+NPROC=`nproc`
+
 # Show commands, expanding variables
 set -x
-# Exit on error
-set -e
 # Do not return wildcards if glob returns no matches
 shopt -s nullglob
 
-(bitcoin-cli getinfo && (bitcoin-cli stop || killall -9 bitcoind)) || true
-sleep 5
+while pgrep -x "bitcoind" > /dev/null
+do
+  echo "Stopping bitcoind..."
+  bitcoin-cli stop
+  sleep 10
+done
+echo "Done."
 
-if [ ! -f ~/clusterizer/chain.json ]; then
-  echo "chain.json not found, running from scratch!"
-  MODE="--new"
-else
+OLDCHAINS=(~/clusterizer/chain.json.old-*)
+
+if [ -e "${OLDCHAINS[0]}" ]; then
   echo "Resuming txoutdump..."
   MODE="--resume"
+  # Determine the last sane chain.json from frozen versions
+  for (( i=${#OLDCHAINS[@]}-1 ; i>=0 ; i-- )) ; do
+    CHAINFILE="${OLDCHAINS[i]}"
+    HASHESLEN=`tail -c 100 "${CHAINFILE}" | cut -d: -f2- | cut -d, -f1`
+    INDEX=`tail -c 100 "${CHAINFILE}" | cut -d: -f3- | cut -d, -f1`
+    if [ $HASHESLEN -eq $INDEX ]; then
+      echo "Last sane chain.json: ${CHAINFILE}, from block ${INDEX}."
+      cp -f "${CHAINFILE}" ~/clusterizer/chain.json
+      break
+    fi
+  done
   cp -f ~/clusterizer/chain.json ~/clusterizer/chain.json.old
-  cp -f ~/clusterizer/chain.json ~/clusterizer/chain.json.old-$(date -Iseconds)
+else
+  echo "Running from scratch!"
+  MODE="--reindex"
 fi
 
-rusty-blockparser -t $(nproc) "${MODE}" --chain-storage ~/clusterizer/chain.json txoutdump ~/clusterizer
+${BLOCKPARSER} -t ${NPROC} ${MODE} --chain-storage ~/clusterizer/chain.json txoutdump ~/clusterizer
 
-for csvfile in ~/clusterizer/tx_out-*.csv
-do
+# Clean chain.json frozen versions older than one week
+find ~/clusterizer -name 'chain.json.old-*' -mtime +7 -exec rm -f {} \;
+
+# Copy chain.json to a frozen version
+cp -f ~/clusterizer/chain.json ~/clusterizer/chain.json.old-$(date -Iseconds)
+
+for csvfile in `find ~/clusterizer -name 'tx_out-*.csv' -mtime -1 -print` ; do
   echo "Sorting ${csvfile}..."
-  LC_ALL=C sort -c "${csvfile}" || LC_ALL=C sort -u --parallel=$(nproc) "${csvfile}" -o "${csvfile}"
+  LC_ALL=C sort -u --parallel=${NPROC} "${csvfile}" -o "${csvfile}"
   echo "Done."
 done
 
 echo "Running clusterizer..."
-rusty-blockparser -t $(nproc) "${MODE}" --chain-storage ~/clusterizer/chain.json.old clusterizer ~/clusterizer
+${BLOCKPARSER} -t ${NPROC} ${MODE} --chain-storage ~/clusterizer/chain.json.old clusterizer ~/clusterizer
+
+rm -f ~/clusterizer/chain.json.old
 
 echo "Sorting clusters.csv..."
-LC_ALL=C sort --parallel=$(nproc) ~/clusterizer/clusters.csv -o ~/clusterizer/clusters.csv
+LC_ALL=C sort --parallel=${NPROC} ~/clusterizer/clusters.csv -o ~/clusterizer/clusters.csv
 echo "Done."
-bitcoind
