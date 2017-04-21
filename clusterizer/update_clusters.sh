a17099c28a0b5035140e9b33d519395554545986
Michele Spagnuolo <mikispag@gmail.com>
2017-04-21 16:08:53 +1200

    Instruct the clusterizer to stop at the previously reached height if .skip-txoutdump is present.

--- clusterizer/update_clusters.sh
+++ clusterizer/update_clusters.sh
@@ -18,30 +18,31 @@ do
 done
 echo "Done."
 
-if [ ! -f ~/clusterizer/.skip-txoutdump ]; then
-  OLDCHAINS=(~/clusterizer/chain.json.old-*)
+OLDCHAINS=(~/clusterizer/chain.json.old-*)
 
+if [ -e "${OLDCHAINS[0]}" ]; then
+  # Determine the last sane chain.json from frozen versions
+  for (( i=${#OLDCHAINS[@]}-1 ; i>=0 ; i-- )) ; do
+    CHAINFILE="${OLDCHAINS[i]}"
+    HASHESLEN=`tail -c 100 "${CHAINFILE}" | cut -d: -f2- | cut -d, -f1`
+    INDEX=`tail -c 100 "${CHAINFILE}" | cut -d: -f3- | cut -d, -f1`
+    if [ $HASHESLEN -eq $INDEX ]; then
+      echo "Last sane chain.json: ${CHAINFILE}, from block ${INDEX}."
+      break
+    fi
+  done
+fi
+
+if [ ! -f ~/clusterizer/.skip-txoutdump ]; then
   if [ -e "${OLDCHAINS[0]}" ]; then
     echo "Resuming txoutdump..."
-    MODE="--resume"
-    # Determine the last sane chain.json from frozen versions
-    for (( i=${#OLDCHAINS[@]}-1 ; i>=0 ; i-- )) ; do
-      CHAINFILE="${OLDCHAINS[i]}"
-      HASHESLEN=`tail -c 100 "${CHAINFILE}" | cut -d: -f2- | cut -d, -f1`
-      INDEX=`tail -c 100 "${CHAINFILE}" | cut -d: -f3- | cut -d, -f1`
-      if [ $HASHESLEN -eq $INDEX ]; then
-        echo "Last sane chain.json: ${CHAINFILE}, from block ${INDEX}."
-        cp -f "${CHAINFILE}" ~/clusterizer/chain.json
-        break
-      fi
-    done
+    cp -f "${CHAINFILE}" ~/clusterizer/chain.json
     cp -f ~/clusterizer/chain.json ~/clusterizer/chain.json.old
   else
     echo "Running from scratch!"
-    MODE="--reindex"
   fi
 
-  ${BLOCKPARSER} -t ${NPROC} ${MODE} --backlog 500 --chain-storage ~/clusterizer/chain.json txoutdump ~/clusterizer
+  ${BLOCKPARSER} -t ${NPROC} --resume --backlog 500 --chain-storage ~/clusterizer/chain.json txoutdump ~/clusterizer
 
   for csvfile in `find ~/clusterizer -name 'tx_out-*.csv' -mtime -1 -print` ; do
     echo "Sorting ${csvfile}..."
@@ -54,16 +55,18 @@ if [ ! -f ~/clusterizer/.skip-txoutdump ]; then
 
   # Clean chain.json frozen versions older than one week
   find ~/clusterizer -name 'chain.json.old-*' -mtime +7 -exec rm -f {} \;
+else
+  # Instruct the clusterizer to stop at the previously reached height
+  CLUSTERIZER_MAX_BLOCK="--max-height ${INDEX}"
 fi
 
 # Create skip-file for txoutdump
 touch ~/clusterizer/.skip-txoutdump
 
 if [ ! -f ~/clusterizer/.skip-clusterizer ]; then
-  MODE="--resume"
   cp -f ~/clusterizer/chain.json.old /tmp/chain.json.old
   echo "Running clusterizer..."
-  ${BLOCKPARSER} -t ${NPROC} ${MODE} --backlog 500 --chain-storage /tmp/chain.json.old clusterizer ~/clusterizer
+  ${BLOCKPARSER} -t ${NPROC} --resume ${CLUSTERIZER_MAX_BLOCK} --backlog 500 --chain-storage /tmp/chain.json.old clusterizer ~/clusterizer
 
   echo "Sorting clusters.csv..."
   LC_ALL=C sort --parallel=${NPROC} ~/clusterizer/clusters.csv -o ~/clusterizer/clusters.csv
