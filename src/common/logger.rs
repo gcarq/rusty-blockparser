980442bf04615dfb0a4062ec4686c854670af8e6
gcarq <egger.m@protonmail.com>
2020-07-15 18:02:09 +0200

    Update time to 0.2.16

---
+++ src/common/logger.rs
@@ -0,0 +1,47 @@
+use std::io::{stderr, stdout, Write};
+
+use log::{self, LogLevel, LogLevelFilter, LogMetadata, LogRecord, SetLoggerError};
+use time::OffsetDateTime;
+
+pub struct SimpleLogger {
+    log_filter: LogLevelFilter,
+}
+
+impl SimpleLogger {
+    pub fn init(log_filter: LogLevelFilter) -> Result<(), SetLoggerError> {
+        log::set_logger(|max_log_level| {
+            max_log_level.set(log_filter);
+            Box::new(SimpleLogger { log_filter })
+        })
+    }
+
+    fn create_log_line(&self, record: &LogRecord) -> String {
+        format!(
+            "[{}] {} - {}: {}\n",
+            OffsetDateTime::now_local().format("%T"),
+            record.level(),
+            record.target(),
+            record.args()
+        )
+    }
+}
+
+impl log::Log for SimpleLogger {
+    fn enabled(&self, metadata: &LogMetadata) -> bool {
+        metadata.level() <= self.log_filter
+    }
+
+    fn log(&self, record: &LogRecord) {
+        if self.enabled(record.metadata()) {
+            let line = self.create_log_line(record);
+            match record.level() {
+                LogLevel::Error => {
+                    stderr().write_all(line.as_bytes()).unwrap();
+                }
+                _ => {
+                    stdout().write_all(line.as_bytes()).unwrap();
+                }
+            }
+        }
+    }
+}
